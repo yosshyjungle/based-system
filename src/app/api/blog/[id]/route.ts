8fc751fd0426302ef40520af4c0f4f306204a09d
yosshyjungle <yosshyjungle@gmail.com>
2025-07-11 12:32:41 +0900

    prisma syuusei2

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "../../../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { main } from "../route";
 
 const prisma = new PrismaClient();
