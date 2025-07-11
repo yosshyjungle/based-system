9892ff906bd6b455d657d4b53a3e9bca12c40e43
yosshyjungle <yosshyjungle@gmail.com>
2025-07-11 10:27:08 +0900

    feature chat add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import ChatContainer from '../components/ChatContainer';
 
 export default function Home() {
   return (
-   <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World!
-  </div>
+    <div className="min-h-screen bg-gray-50">
+      <ChatContainer />
+    </div>
   );
 }
